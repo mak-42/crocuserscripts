402903665e9d216ea131066fae55747e17fd4af7
Dmitry <mak-42@users.noreply.github.com>
2025-09-10 11:44:33 +0300

    Create fix_message_panel_styles.user.js

---
+++ telegram/fix_message_panel_styles.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Fix Telegram A wide screen styles
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       Dmitry Malenok
+// @match        https://web.telegram.org/a/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=telegram.org
+// @grant        none
+// ==/UserScript==
+
+function GM_addStyle(css) {
+  const style = document.getElementById("GM_addStyleByCrocJiraFix") || (function() {
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.id = "GM_addStyleByCrocJiraFix";
+    document.head.appendChild(style);
+    return style;
+  })();
+  const sheet = style.sheet;
+  sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
+}
+
+(function() {
+    'use strict';
+
+    GM_addStyle('.MessageList .messages-container { max-width: unset !important; width: unset !important; }');
+    GM_addStyle('.message-content { max-width: 45vw !important; width: unset !important; }');
+})();
