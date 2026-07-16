2f21f2e540c2b5708fe80c0622ebbca47a7dbee3
Dmitry <mak-42@users.noreply.github.com>
2026-07-16 09:17:53 +0300

    Update remove_phone_request.user.js
    Delay was added

--- talk/remove_phone_request.user.js
+++ talk/remove_phone_request.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Talk: Remove Phone Request
 // @namespace    http://tampermonkey.net/
-// @version      1.4.0
+// @version      1.3.2
 // @description  Remove Phone Request from Talk
 // @author       Dmitry Malenok
 // @match        https://talks.croc.ru/*
@@ -18,12 +18,26 @@
     var remover = () => {
         if ($("span:contains('Добавьте номер телефона')").length) {
             $('button:has(span:contains("Введите номер телефона"))').click();
-            $('button:has(span:contains("Отмена"))').click();
-            $('.user-settings-header > button').click()
+            var remover21 = () => {
+                if ($("span:contains('Отмена')").length) {
+                    $('button:has(span:contains("Отмена"))').click();
+                    $('.user-settings-header > button').click()
+                } else {
+                    setTimeout(remover21, 500);
+                }
+            }
+            remover21();
         } else if ($("span:contains('Add a phone number')").length) {
             $('button:has(span:contains("Enter the number"))').click();
-            $('button:has(span:contains("Cancel"))').click();
-            $('.user-settings-header > button').click()
+            var remover22 = () => {
+                if ($("span:contains('Cancel')").length) {
+                    $('button:has(span:contains("Cancel"))').click();
+                    $('.user-settings-header > button').click()
+                } else {
+                    setTimeout(remover22, 500);
+                }
+            }
+            remover22();
         } else {
             setTimeout(remover, 500);
         }
